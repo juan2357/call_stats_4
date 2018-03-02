bae3c301c9eb1f7cd7bb5b04e429328bb87fca6e
juanfperez <juanfperez@me.com>
2018-03-02 04:24:57 -0500

    cleaned up some whitespace. left notes from instructor for future reference

--- call_stats4.cpp
+++ call_stats4.cpp
@@ -8,10 +8,6 @@ Total Points: 20
 Assignment 6: call_stats4.cpp
 
 Description:
-
-
-Note:  In this assignment you will
-
 1. Read the contents of a datafile one record at a time into an array of
    records;
 2. Process the data stored in an array of records;
@@ -67,13 +63,11 @@ void Input(call_record call_DB[], int & count)
 
 	in.open("call_data.txt"); //bind the file "call_data.txt" to the input
 	                          //file stream "in".
-
 	if (in.fail()) //if file not found print message and exit program
 	{
 		cout << "Input file did not open correctly" << endl;
 		exit(1);
 	}
-
 	//Remember to use a while loop when reading from a file because
 	//you do not know how many records you will be reading.
 	while (!in.eof())
@@ -83,25 +77,21 @@ void Input(call_record call_DB[], int & count)
 		in >> call_DB[count].call_length;
 		count++;
 	}
-
-
 	in.close();
 }
-
 ///*************************************************************************************
 //Name:  Output
-//Precondition:
-//Postcondition:
-//
-//Description:
+//Precondition: The variables have been initialized and calculated
+//Postcondition: Results are displayed.
+//Description: Prints results from user input and calculations to a file
 //*************************************************************************************
-
 void Output(const call_record call_DB[], const int & count)
 {
 	ofstream out; //declare the output file stream "out".
 
 	out.open("weekly4_call_info.txt");//bind the file "weekly4_call_info.txt" to
 	                       //to the output file stream "out".
+	//Magic Formula
 	out.setf(ios::showpoint);
  	out.precision(2);
  	out.setf(ios::fixed);
@@ -113,7 +103,7 @@ void Output(const call_record call_DB[], const int & count)
 	}
   // use a "for" loop here to
 	// print the output to file
-	for (i = 0; i < count; i++) {
+	for (int i = 0; i < count; i++) {
 		out<< std::left << setw(30)<< "Cell Phone " <<call_DB[i].cell_number<<"  "<<endl;
 		out<< std::left << setw(30)<< "Number of Relay Stations " <<call_DB[i].relays<<"   "<<endl;
 		out<< std::left << setw(30)<< "Minutes Used " <<call_DB[i].call_length<<endl;
@@ -121,35 +111,30 @@ void Output(const call_record call_DB[], const int & count)
 	  out<< std::left << setw(30)<< "Tax Rate " <<call_DB[i].tax_rate<<endl;
 	  out<< std::left << setw(30)<< "Call Tax " <<call_DB[i].call_tax<<endl;
 	  out<< std::left << setw(30)<< "Total Cost of Call " <<call_DB[i].total_cost<<endl<<endl;
-
 	}
 	out.close();
 }
 
 ///*************************************************************************************
 //Name:  Process
-//Precondition:
-//Postcondition:
-//Description:
+//Precondition: The variables have been initialized by the data file.
+//Postcondition: Conditional statements initialize relay variables.
+//Description: Calculates net_cost, call_tax and total_cost.
 //*************************************************************************************
 
 void Process(call_record call_DB[], const int & count)
 {
-	int i; // index for the "for" loop..
-
 	//Remember to use a "for" loop when you know how many items
 	//you will be processing.  Use the dot operator when you are
 	//accessing fields in a record.  For example, when we need
 	//to access the relay field in the first record in the array use
-	//call_DB[0].relay   .
-
-	for (i = 0; i < count; i++)
+	//call_DB[0].relay
+	for (int i = 0; i < count; i++)
 	{
 		call_DB[i].net_cost = call_DB[i].relays / 50.0 * .40 * call_DB[i].call_length;
 		call_DB[i].call_tax = call_DB[i].net_cost * call_DB[i].tax_rate;
 	  call_DB[i].total_cost = call_DB[i].net_cost + call_DB[i].call_tax;
-
-
+		//Conditional statements to determine tax rates
 		if (call_DB[i].relays <= 0 && call_DB[i].relays <=5) {
 	    call_DB[i].tax_rate = 0.01;
 	  } else if (call_DB[i].relays <= 6 && call_DB[i].relays <=11) {
@@ -161,12 +146,7 @@ void Process(call_record call_DB[], const int & count)
 	  } else {
 	    call_DB[i].tax_rate = 0.12;
 	  }
-
-                             // ADD THE REST OF THE CODE TO PROCESS a call_DB[i] record
-
-
 	}
-
 }
 
 int main()
@@ -179,10 +159,8 @@ int main()
 	                       //into the array call_DB
 	Process(call_DB, count); //this function will process each call_record stored
 	                       //in the array call_DB.
-
 	Output(call_DB, count);//this function prints all the fields
 	                       //of every record stored in the array call_DB to an output file.
 	                       //Each record with be print on a separate line in the output file.
-
-		return 0;
+	return 0;
 }
